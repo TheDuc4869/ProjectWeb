dab88d2e07c0e8660da1c3e188bd6a9bbc7d8124
TheDuc4869 <22dh110825@st.huflit.edu.vn>
2023-10-24 12:03:03 +0700

    Add project files.

---
+++ ProjectWeb/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Watch()
+        {
+            return View();
+        }
+        public ActionResult Mac()
+        {
+            return View();
+        }
+        public ActionResult IPad()
+        {
+            return View();
+        }
+        public ActionResult IPhone()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
